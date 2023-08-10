9adb08879fbd34ca51fd91e904dc4a2c9900989a
David Dansby <39511285+DataDavD@users.noreply.github.com>
2023-08-10 00:45:08 -0700

    Finalize datadavd Lulu keymap

---
+++ keyboards/boardsource/lulu/keymaps/datadavd/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/* Select hand configuration */
+#define MASTER_LEFT
+
+/* Mouse Key configs */
+#define MOUSEKEY_DELAY 50
--- keyboards/boardsource/lulu/rp2040/rules.mk
+++ keyboards/boardsource/lulu/rp2040/rules.mk
@@ -1,2 +1,1 @@
 SERIAL_DRIVER = vendor
-BOOTLOADER = rp2040
